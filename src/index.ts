1d507c710045b38148c9719f43a4fab2a73506b2
Nick Galante <nicholasgalante1997@gmail.com>
2023-07-07 17:05:17 -0600

    runs server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { server } from './server';
+import { logger } from './logger';
 
 function initApp() {
   try {
+    logger.info('Starting service Paldea Evolved...');
     server.listen();
   } catch (e: any) {
-
+    logger.error(e);
   } finally {
-
+    logger.info('Stopping Paldea-Evolved server...');
   }
 }
+
+initApp();
